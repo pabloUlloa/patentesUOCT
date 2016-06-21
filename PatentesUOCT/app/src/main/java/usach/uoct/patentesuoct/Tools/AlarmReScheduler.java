2d194ec96343dbffe8bea09f19595c54e62c0f4f
callmezarri <nicolas.munoz.z@usach.cl>
2016-06-20 23:08:53 -0400

    pablo estoy probando la wea

---
+++ PatentesUOCT/app/src/main/java/usach/uoct/patentesuoct/Tools/AlarmReScheduler.java
@@ -0,0 +1,15 @@
+package usach.uoct.patentesuoct.Tools;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by zarrizarri on 21-06-16.
+ */
+public class AlarmReScheduler extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String s = "el pablo chupa el sendo pico";
+    }
+}
